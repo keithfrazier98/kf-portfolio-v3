167eadd0d23928437b037c95d7bae20413c48696
Keith Frazier <keithers98@gmail.com>
2022-03-31 15:55:19 -0700

    revised responsive styling for contact page

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -22,30 +22,9 @@ export default function ContactMe() {
     }
   }, [somethingCopied]);
   return (
-    <div className="absolute top-0 z-20 w-full h-full py-24 px-32">
-      <div className="w-full h-full flex items-center justify-around">
-        <div className="rounded-lg p-[2px] redBlue">
-          <div className="bg-black rounded-lg">
-            <div className="flex flex-col items-center rounded burgersAndFries p-4">
-              <p className="pb-2 text-xl font-bold text-white">My Locality</p>
-              <div className="rainbow-sdws p-[2px] rounded-lg">
-                <div className="bg-black rounded-lg">
-                  <div className="burgersAndFries rounded-lg">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d845781.5973462377!2d-118.17143572126591!3d34.09938342269121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdbd6107807ace273!2zMzTCsDA1JzU3LjgiTiAxMTfCsDM2JzQwLjEiVw!5e0!3m2!1sen!2sus!4v1648765505015!5m2!1sen!2sus"
-                      width="600"
-                      height="450"
-                      className="rounded-lg border-0"
-                      loading="lazy"
-                      referrerpolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center">
+    <div className="absolute top-0 z-20 w-full h-full py-24 px-16 hide-scrollbar 2xl:px-32 overflow-scroll">
+      <div className="w-full h-full flex items-center justify-around flex-col xl:flex-row">
+        <div className="flex flex-col items-center justify-center mb-6 pt-96 xl:pt-0">
           <div className="pb-2 relative">
             <div
               className={`absolute -top-20 left-10  ${somethingCopied ? "block" : "hidden"} ${
@@ -118,6 +97,27 @@ export default function ContactMe() {
                   <div className="p-2 w-full h-full bg-black bg-opacity-100 hover:bg-opacity-10 rounded">Submit</div>
                 </button>
               </form>
+            </div>
+          </div>
+        </div>
+        <div className="pb-28 xl:pb-0">
+          <div className="rounded-lg p-[2px] redBlue">
+            <div className="bg-black rounded-lg">
+              <div className="flex flex-col items-center rounded burgersAndFries p-4">
+                <p className="pb-2 text-xl font-bold text-white">My Locality</p>
+                <div className="rainbow-sdws p-[2px] rounded-lg">
+                  <div className="bg-black rounded-lg">
+                    <div className="burgersAndFries rounded-lg">
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d845781.5973462377!2d-118.17143572126591!3d34.09938342269121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdbd6107807ace273!2zMzTCsDA1JzU3LjgiTiAxMTfCsDM2JzQwLjEiVw!5e0!3m2!1sen!2sus!4v1648765505015!5m2!1sen!2sus"
+                        className="rounded-lg border-0 w-[400px] h-[300px] lg:w-[600px] lg:h-[450px]"
+                        loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
