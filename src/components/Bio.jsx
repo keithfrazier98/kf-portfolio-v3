67ce2f0de0a3aa09315a065fb11fa93d65980174
Keith Frazier <keithers98@gmail.com>
2022-08-23 07:16:31 -0700

    minor style update

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,7 +4,7 @@ import OffsetBorder from "./OffsetBorder";
 
 export default function Bio() {
   return (
-    <div id="about" className="bg-white py-32 dark:bg-black flex flex-col justify-center items-center dark:border-white border-black border-y">
+    <div id="about" className="bg-white py-32 dark:bg-black flex flex-col md:flex-row justify-center items-center dark:border-white border-black border-y">
       <p className="max-w-2xl h-fit mx-12 mb-6">
         I am a web developer with a long time interest in creating amazing things with computers. With a focus on front-end engineering, I also like to dabble with producing music,
         graphic design, and creating 3D models for printing or machining. Feel free to check out my experience and stack through the links above, or contact me if you'd like to
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,12 @@
-export default function NotFound () {
-    
+import OffsetBorder from "./OffsetBorder";
+
+export default function NotFound() {
+  return (
+    <div className="p-12 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+      <div className="w-full h-full relative">
+        <p>404, nothing here to see!</p>
+        <OffsetBorder offsetPx={12} shadow="blur" />
+      </div>
+    </div>
+  );
 }
