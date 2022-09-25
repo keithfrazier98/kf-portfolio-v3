0d0cc2a7948f9f82a73cd45abd3db17646ad6d75
Keith Frazier <keithers98@gmail.com>
2022-09-25 14:47:24 -0700

    improved contact me interactivity

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import OffsetBorder from "./OffsetBorder";
+import pic from "../images/keith.jpg"
 
 export default function Bio() {
   return (
     <div
-      className="bg-white absolute py-24 top-0 left-0 right-0 bottom-0 dark:bg-black flex flex-col md:flex-row justify-center items-center dark:border-white border-black border-y"
+      className="bg-white absolute md:p-24 top-0 left-0 right-0 bottom-0 dark:bg-black flex flex-col lg:flex-row justify-center items-center dark:border-white border-black border-y"
     >
       <p className="max-w-2xl h-fit mx-12 mb-6">
         I am a web developer with a long time interest in creating amazing things with computers. With a focus on front-end engineering, I also like to dabble with producing music,
         graphic design, and creating 3D models for printing or machining. Feel free to check out my experience and stack through the links above, or contact me if you'd like to
         chat!
       </p>
-      <div className="h-64 w-64 md:h-96 md:w-96 bg-pic bg-cover relative bg-no-repeat">
+      <div className="flex relative max-w-sm">
+        <img src={pic} alt="keith" />
         <OffsetBorder offsetPx={"6"} shadow={"solid"} />
       </div>
     </div>
--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -8,55 +8,38 @@ import OffsetBorder from "./OffsetBorder";
 
 export default function ContactMe() {
   const [state, handleSubmit] = useForm("xnqlryyy");
-  const [notification, setNotification] = useState(null);
-  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [copiedGH, setCopiedGH] = useState(false);
+  const [copiedEmail, setCopiedEmail] = useState(false);
+  const [btnText, setBtnText] = useState("Send");
 
   useEffect(() => {
     if (state.succeeded) {
-      setNotification("Email sent!");
-      state.succeeded = false;
+      setBtnText("Success!");
+      setTimeout(() => {
+        setBtnText("Send");
+      }, 1500);
     } else if (state.errors.length > 0) {
-      setNotification("Oops! Email failed to send!");
+      setBtnText("Hmm, an error occurred. Please try again.");
+      setTimeout(() => {
+        setBtnText("Send");
+      }, 1500);
     }
-  }, [state.submitting, state.succeeded, state.errors]);
-
-  useEffect(() => {
-    if (notification) {
-      setTimeout(() => {
-        setShowSnackbar(true);
-      }, 150);
-      setTimeout(() => {
-        setShowSnackbar(false);
-      }, 2500);
-      setTimeout(() => {
-        setNotification(null);
-      }, 3000);
-    }
-  }, [notification]);
+  }, [state.succeeded, state.errors, state.submitting]);
 
   return (
-    <div className="relative w-full h-full bg-gray-100 dark:bg-zinc-900">
+    <>
       <div id="contact" className="w-full flex items-center pt-64 md:pt-0 dark:bg-zinc-900 bg-gray-100 overflow-hidden">
         <div className="text-black dark:text-white w-full xl:items-end flex justify-around items-center flex-col xl:flex-row my-12">
-          <div className="relative w-min my-10 xl:m-0 mx-4">
+          <div className="relative w-min flex max-w-lg my-10 xl:m-0 mx-4">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d845781.5973462377!2d-118.17143572126591!3d34.09938342269121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdbd6107807ace273!2zMzTCsDA1JzU3LjgiTiAxMTfCsDM2JzQwLjEiVw!5e0!3m2!1sen!2sus!4v1648765505015!5m2!1sen!2sus"
-              className="border w-full relative z-10 shadow-lg border-black dark:border-white h-[300px] lg:w-[600px] lg:h-[450px]"
+              className="border w-64 h-64 sm:w-96 sm:h-96 xl:w-[30rem] xl:h-[30rem] relative z-10 shadow-lg border-black dark:border-white"
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
             <OffsetBorder offsetPx={"12"} shadow="solid" />
           </div>
-          <div className={`transition-opacity duration-300 flex flex-col items-center justify-center xl:pt-0 mb-12 md:mb-0 w-full md:w-auto px-2`}>
-            <div
-              className={`absolute top-0 left-0 -translate-y-full ${notification ? "block" : "hidden"} ${
-                showSnackbar ? "opacity-100 translate-x-0" : "translate-x-full opacity-0"
-              } transition-all duration-500`}
-            >
-              <div className="bg-black px-10 py-2">
-                <p className="text-white w-max">{notification}</p>
-              </div>
-            </div>
+          <div className={`transition-opacity duration-300 flex flex-col items-center justify-center xl:pt-0 mb-12 md:mb-0 w-full md:w-auto px-6`}>
             <div className="pb-2 z-10 relative">
               <div className="grid grid-flow-col gap-6 my-6">
                 <a href="https://www.linkedin.com/in/keith-frazier/" target="_blank" className="relative group mr-6">
@@ -74,19 +57,35 @@ export default function ContactMe() {
                       className="relative group mx-6 my-6"
                       onClick={() => {
                         navigator.clipboard.writeText("keithers98@gmail.com");
-                        setNotification("Email address copied to clipboard!");
+                        setCopiedEmail(true);
+                        setTimeout(() => setCopiedEmail(false), 750);
                       }}
                     >
+                      <span
+                        className={`absolute bottom-0 left-1/2 -translate-x-1/2 duration-200 transition-all ${
+                          copiedEmail ? "-translate-y-[125%] opacity-100" : "translate-y-full opacity-0"
+                        }`}
+                      >
+                        Copied!
+                      </span>
                       <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-12 h-12 transition-colors duration-300 group-hover:bg-red-600" />
                       <ImGoogle2 className="w-14 h-16 hover:text-white transition-colors duration-300 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-30" />
                     </button>
                     <button
                       className="relative group mx-6"
                       onClick={() => {
-                        navigator.clipboard.writeText("Safari | DataX#2063");
-                        setNotification("Discord tag copied to clipboard!");
+                        navigator.clipboard.writeText("keithers98#2063");
+                        setCopiedGH(true);
+                        setTimeout(() => setCopiedGH(false), 500);
                       }}
                     >
+                      <span
+                        className={`absolute top-0 left-1/2 -translate-x-1/2 duration-200 transition-all ${
+                          copiedGH ? "-translate-y-[125%] opacity-100" : "translate-y-full opacity-0"
+                        }`}
+                      >
+                        Copied!
+                      </span>
                       <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-10 h-8 transition-colors duration-300 group-hover:bg-white" />
                       <BsDiscord className="w-14 h-16 hover:text-indigo-600 transition-colors duration-300 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-30" />
                     </button>
@@ -105,7 +104,7 @@ export default function ContactMe() {
                 <ValidationError prefix="Message" field="message" errors={state.errors} />
                 <button className="btnReg" disabled={state.submitting}>
                   <div className="p-2 w-full h-full flex justify-center items-centerm-0">
-                    <div className="flex">{state.submitting ? <MoonLoader size={24} color={"white"} /> : "Submit"}</div>
+                    <div className="flex">{state.submitting ? <MoonLoader size={18} className="text-black dark:text-white" /> : btnText}</div>
                   </div>
                 </button>
               </form>
@@ -114,6 +113,6 @@ export default function ContactMe() {
           </div>
         </div>
       </div>{" "}
-    </div>
+    </>
   );
 }
